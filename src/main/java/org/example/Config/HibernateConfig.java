b47c9d45fd6538c57ca2fc74865547515ded973f
ETHMUNI <sense1xnft@gmail.com>
2024-05-09 10:50:51 +0200

    Update HibernateConfig.java

--- src/main/java/org/example/Config/HibernateConfig.java
+++ src/main/java/org/example/Config/HibernateConfig.java
@@ -90,7 +90,7 @@ public class HibernateConfig {
             props.put("hibernate.connection.password", "postgres");
             props.put("hibernate.archive.autodetection", "class");
             props.put("hibernate.show_sql", "true");
-            props.put("hibernate.hbm2ddl.auto", "create-drop");
+            props.put("hibernate.hbm2ddl.auto", "update");
             return getEntityManagerFactory(configuration, props);
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
